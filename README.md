31178b6505a55dd5c18505688f254d599021cd3b
fuadnafiz98 <fuadnafiz98@gmail.com>
2023-01-14 12:46:03 +0700

    setting up database

--- README.md
+++ README.md
@@ -8,7 +8,39 @@ go get github.com/jackc/pgx/v5/pgxpool # this supports concurrency that is helpf
 
 - Installing sqlc
   - `brew install sqlc`
-  - ``
+
+## Generating Bulk data
+
+In this repo I will write some scripts to generate large ammount of data.
+
+### Problem description
+
+1. Querying large ammount of data with joining multiple tables
+2. joining multiple tables as well as aggigare data
+3. effects before and after indexing
+4. searching text, fuzzy searching
+
+Let's consider a relatively complex(!) system where we have to manage a large amount of blogs that publish on different websites, websites hosted on different hosting platforms,
+
+```bash
+Blog:
+  - ID
+  - Title
+  - URL
+  - Status ---> (Requested, Review, Approved, Integrated, Published, Deleted)
+  - Date Published
+  - Website
+  - Hosting
+  - Writer             |
+  - Editor             |
+  - Publisher          | --> Actors
+  - Webmaster          |
+  - Hosting Admin      |
+```
+
+We will add the tags, category etc later
+
+Head over to the `gen` folder for the codes to generate sql query that will prepare the database.
 
 ## The Postgres toolkit
 
---
+++ gen/sql/create.sql
@@ -0,0 +1,106 @@
+--- Tag
+CREATE TABLE IF NOT EXISTS tag (
+  tag_id SERIAL PRIMARY KEY,
+  tag_name varchar(100) NOT NULL,
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+--- Writer
+CREATE TABLE IF NOT EXISTS writer (
+  writer_id SERIAL PRIMARY KEY,
+  writer_username varchar(100) NOT NULL,
+  writer_email varchar(400) NOT NULL,
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+--- Editor
+CREATE TABLE IF NOT EXISTS editor (
+  editor_id SERIAL PRIMARY KEY,
+  editor_username varchar(100) NOT NULL,
+  editor_email varchar(400) NOT NULL,
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+--- Webmaster
+CREATE TABLE IF NOT EXISTS webmaster (
+  webmaster_id SERIAL PRIMARY KEY,
+  webmaster_username varchar(100) NOT NULL,
+  webmaster_email varchar(400) NOT NULL,
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+--- Website
+CREATE TABLE IF NOT EXISTS website (
+  website_id SERIAL PRIMARY KEY,
+  website_url varchar(2048) NOT NULL,
+  webmaster_id int,
+  CONSTRAINT fk_webmaster FOREIGN KEY(webmaster_id) REFERENCES webmaster(webmaster_id),
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+--- Host Admin
+CREATE TABLE IF NOT EXISTS hosting_admin (
+  hosting_admin_id SERIAL PRIMARY KEY,
+  hosting_admin_username varchar(100) NOT NULL,
+  hosting_admin_email varchar(400) NOT NULL,
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+--- Hosting
+CREATE TABLE IF NOT EXISTS hosting (
+  hosting_id SERIAL PRIMARY KEY,
+  hosting_name varchar(2048) NOT NULL,
+
+  host_admin_id int,
+  CONSTRAINT fk_host_admin FOREIGN KEY(host_admin_id) REFERENCES hosting_admin(hosting_admin_id),
+
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+
+--- Blog
+CREATE TABLE IF NOT EXISTS blog (
+  blog_id SERIAL PRIMARY KEY,
+  blog_title varchar(100) NOT NULL,
+  blog_url varchar(2048) NOT NULL UNIQUE,
+  blog_status varchar(100) NOT NULL,
+  blog_approved_at timestamp,
+  blog_published_at timestamp,
+
+  blog_writer_id int,
+  CONSTRAINT fk_blog_writer FOREIGN KEY(blog_writer_id) REFERENCES writer(writer_id),
+
+  blog_editor_id int,
+  CONSTRAINT fk_blog_editor FOREIGN KEY(blog_editor_id) REFERENCES editor(editor_id),
+
+  blog_website_id int,
+  CONSTRAINT fk_blog_website FOREIGN KEY(blog_website_id) REFERENCES website(website_id),
+
+  blog_hosting_id int,
+  CONSTRAINT fk_blog_hosting FOREIGN KEY(blog_hosting_id) REFERENCES hosting(hosting_id),
+
+  blog_created_at timestamp NOT NULL DEFAULT NOW(),
+  blog_updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS blog_tag (
+  tag_id int,
+  blog_id int,
+
+  primary key (tag_id, blog_id),
+
+  CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tag(tag_id),
+  CONSTRAINT fk_blog FOREIGN KEY(blog_id) REFERENCES blog(blog_id)
+);
